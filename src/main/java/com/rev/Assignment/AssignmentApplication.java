7755963057a4d7e95de68f7b1712d9eca470513f
devesh <devesh9494@gmail.com>
2020-05-21 17:40:07 +0530

    Handle 500 Error on res absent

--- src/main/java/com/rev/Assignment/AssignmentApplication.java
+++ src/main/java/com/rev/Assignment/AssignmentApplication.java
@@ -3,11 +3,26 @@ package com.rev.Assignment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class AssignmentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AssignmentApplication.class, args);
+        checkRes();
     }
 
+    public static void checkRes() {
+        try {
+            File f=new File("./res");
+            if(!f.exists())
+                f.mkdir();
+            File f2=new File("./res/files");
+            if(!f2.exists())
+                f2.mkdir();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
